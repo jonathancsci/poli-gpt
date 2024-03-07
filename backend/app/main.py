a6888fa8936854a539620f851b85521fda00c8b4
Logan Assalley <loas7229@colorado.edu>
2024-03-07 12:02:14 -0700

    feat: added generate funcitonality to frontend
    also had to edit main.py to work with cors.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import generate, search
 
 app = FastAPI()
 app.include_router(generate.router)
 app.include_router(search.router)
+
+
+origins = [
+    "http://127.0.0.1:5500"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount("/js", StaticFiles(directory="../frontend/js"), name="js")
 
