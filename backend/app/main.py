29bccd015348e67a0318fb811b048496829b459b
Jonathan Wu <155753916+jonathancsci@users.noreply.github.com>
2024-03-31 16:14:18 -0600

    Remove cors middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-from fastapi.middleware.cors import CORSMiddleware
 from .data.init_db import init_db
 from .routers import generate, search
 
@@ -10,19 +9,6 @@ init_db()
 app = FastAPI()
 app.include_router(generate.router)
 app.include_router(search.router)
-
-
-origins = [
-    "http://127.0.0.1:5500"
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 #app.mount("/js", StaticFiles(directory="../frontend/js"), name="js")
 app.mount("/frontend", StaticFiles(directory="../frontend"), name="frontend")
